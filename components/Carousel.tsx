60a977f4d7c65d8cf3b38bcba94b0beb3a5b6274
MindTheSnack <mindthesnackgh@gmail.com>
2026-05-02 19:10:57 +0200

    fix [compiler error] unused variables

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import Modal from './Modal'
 
 const carouselProducts = [
